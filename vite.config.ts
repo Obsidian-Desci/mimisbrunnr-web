d13447414aed5de09264fe5b23ce98526348eda5
Taylor Hulsmans <thulsmans.1133@gmail.com>
2023-11-02 16:27:29 -0700

    base in vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import * as path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/mimisbrunnr-web/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, './src')
